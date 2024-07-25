e84eb29a47f6a945b5313d7b43554e3ff9b886f4
Tom <tommy_brown@me.com>
2024-07-25 16:12:52 +0100

    Added all dummy org data to scripts/generate-organisations.js

--- scripts/generate-organisations.js
+++ scripts/generate-organisations.js
@@ -5,14 +5,72 @@ const path = require('path')
 const faker = require('@faker-js/faker').faker
 
 
-const generateOrganisations = () => {
-    return []
+
+const generateOrganisation = () => {
+  let organisation = {}
+
+  
+  organisation.details = {}
+
+  //Organisation name
+  organisation.details.name = faker.company.name()
+
+  //UK provider reference number (UKPRN)
+  organisation.details.ukprn = faker.string.numeric(8)
+
+  //Unique reference number (URN)
+  organisation.details.urn = faker.string.numeric(6)
+
+  //Agreed by
+  organisation.details.firstName = faker.person.firstName()
+  organisation.details.lastName = faker.person.lastName()
+
+  //Agreed on
+  organisation.details.date = faker.date.between({ from: '2020-01-01T00:00:00.000Z', to: '2024-01-01T00:00:00.000Z' }) // '2026-05-16T02:22:53.002Z'
+
+  //Region
+  organisation.details.region = faker.location.county()
+
+  //Hourly rate
+
+  //Email
+  organisation.details.email = emailAddress = `${organisation.details.firstName.toLowerCase()}.${organisation.details.lastName.toLowerCase()}@ed.co.uk`
+
+  //Tel
+  
+
+  //Website
+  organisation.details.website = faker.internet.domainName()
+  
+  //Address
+  organisation.details.address = {
+    line1: faker.location.buildingNumber() + " " + faker.location.street(),
+    town: faker.location.city(),
+    postcode: 'W9 1ST'
+  }
+
+
+  return organisation
 }
 
 
 
 
-const generateOrganisationsFile = (filePath) => {
+const generateOrganisations = () => {
+    
+  const organisations = []
+  
+    for(let i = 0; i < 100; i++) {
+        organisations.push(generateOrganisation())
+    }
+
+    return organisations
+}
+
+
+
+
+  const generateOrganisationsFile = (filePath) => {
     const organisations = generateOrganisations()
     const filedata = JSON.stringify(organisations, null, 2)
     fs.writeFile(
