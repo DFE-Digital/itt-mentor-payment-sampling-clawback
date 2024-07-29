6affe258f2a3a3f927c284498ac8630ad10754d6
Tom <tommy_brown@me.com>
2024-07-29 18:03:11 +0100

    added placeholder content to mark complete and clawback screens and added MOJ style header actions on claims/index and claims/sampling

--- app/routes/claims.js
+++ app/routes/claims.js
@@ -4,22 +4,19 @@ module.exports = router => {
 
  router.get('/claims', (req, res) => {
   let organisations = req.session.data.organisations
+  
   let selectedStatusFilters = _.get(req.session.data.filters, 'statuses')
 
   if(_.get(selectedStatusFilters, 'length')) {
-    selectedFilters.categories.push({
-      heading: { text: 'Status' },
-      items: selectedStatusFilters.map(label => {
-        return {
-          text: label,
-          href: `/organisations/remove-status/${label}`
-        }
-      })
+    organisations = organisations.details.filter(organisation => {
+      let matchesStatus = true
+
+      matchesStatus = selectedStatusFilters.includes(organisation.claim)
+ 
+      return matchesStatus
     })
   }
-
-
-
+  
   res.render('claims/index', {
     organisations
   })
--- scripts/generate-organisations.js
+++ scripts/generate-organisations.js
@@ -10,14 +10,14 @@ const generateOrganisation = () => {
   let organisation = {}
 
   
-  organisation.details = {}
+  
 
   //Organisation ID
-  organisation.details.id = "" + faker.number.int({ min: 123456, max: 999999 })
+  organisation.id = "" + faker.number.int({ min: 123456, max: 999999 })
 
 
   //Stautuses
-  organisation.details.claim = faker.helpers.arrayElement([
+  organisation.claim = faker.helpers.arrayElement([
     'Submitted',
     'Draft',
     'Checking',
@@ -32,52 +32,52 @@ const generateOrganisation = () => {
   ])
 
   //Providers
-  organisation.details.provider = faker.helpers.arrayElement([
+  organisation.provider = faker.helpers.arrayElement([
     'Best Practice Network',
     'National Institute of Teaching'
   ])
   
   //Organisation uuid
-  organisation.details.uuid = faker.string.uuid()
+  organisation.uuid = faker.string.uuid()
 
   //School name
-  organisation.details.name = faker.company.buzzAdjective() + " school"
+  organisation.name = faker.company.buzzAdjective() + " school"
   
 
   //Organisation Ref number append to school name
-  organisation.details.claimRef = faker.number.int({ min: 123456, max: 999999 }) + " - " + organisation.details.name
+  organisation.claimRef = faker.number.int({ min: 123456, max: 999999 }) + " - " + organisation.name
 
   //UK provider reference number (UKPRN)
-  organisation.details.ukprn = faker.string.numeric(8)
+  organisation.ukprn = faker.string.numeric(8)
 
   //Unique reference number (URN)
-  organisation.details.urn = faker.string.numeric(6)
+  organisation.urn = faker.string.numeric(6)
 
   //Agreed by
-  organisation.details.firstName = faker.person.firstName()
-  organisation.details.lastName = faker.person.lastName()
+  organisation.firstName = faker.person.firstName()
+  organisation.lastName = faker.person.lastName()
 
   //DAte
-  organisation.details.date = faker.date.recent({ days: 300 })
+  organisation.date = faker.date.recent({ days: 300 })
 
   //Region
-  organisation.details.region = faker.location.county()
+  organisation.region = faker.location.county()
 
   //Cost
-  organisation.details.cost = faker.commerce.price({ min: 100, max: 10000, symbol: '£' })
+  organisation.cost = faker.commerce.price({ min: 100, max: 10000, symbol: '£' })
 
   //Email
-  organisation.details.emailAddress = `${organisation.details.firstName.toLowerCase()}.${organisation.details.lastName.toLowerCase()}@${organisation.details.name.toLowerCase()}.uk`
+  organisation.emailAddress = `${organisation.firstName.toLowerCase()}.${organisation.lastName.toLowerCase()}@${organisation.name.toLowerCase()}.uk`
 
   //Tel
-  organisation.details.phone = faker.phone.number()
+  organisation.phone = faker.phone.number()
 
   //Website
-  organisation.details.website = faker.internet.domainName()
+  organisation.website = faker.internet.domainName()
   
   
   //Address
-  organisation.details.address = {
+  organisation.address = {
     line1: faker.location.buildingNumber() + " " + faker.location.street(),
     town: faker.location.city(),
     postcode: 'W9 1ST'
