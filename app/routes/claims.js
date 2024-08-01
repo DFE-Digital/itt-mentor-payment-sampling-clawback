1f3fb6f433d542ffb6bf652350d51cfb3a196e08
Tom <tommy_brown@me.com>
2024-08-01 13:50:49 +0100

    Added working status filter for claims/index in claims.js

--- app/routes/claims.js
+++ app/routes/claims.js
@@ -1,11 +1,58 @@
+const _ = require('lodash')
 module.exports = router => {
-  
+
+  // Claims Filters
+   router.get('/claims', (req, res) => {
+    let claims = req.session.data.claims
+
+    let selectedStatusFilters = _.get(req.session.data.filters, 'statuses')
+
+    let selectedFilters = {
+      categories: []
+    }
+
+    if (_.get(selectedStatusFilters, 'length')) {
+      claims = claims.filter(claim => {
+        let matchesStatus = true
+        
+        matchesStatus = selectedStatusFilters.includes(claim.status);
+
+        return matchesStatus
+      })
+
+      selectedFilters.categories.push({
+        heading: {text: 'status'},
+        items: selectedStatusFilters.map(label => {
+          return{
+            text: label,
+            href: `/claims/remove-status/${label}`  
+          }
+        })
+      }) 
+  }
+
+  res.render('claims/index', {
+    claims,
+    selectedFilters
+
+  })
+})
+
+  //Remove filter
+  router.get('/claims/remove-status/:status', (req, res) => {
+    _.set(req, 'session.data.filters.statuses',  _.pull(req.session.data.filters.statuses, req.params.status))
+    res.redirect('/claims')
+  })
+
+    
+  //Claims list
   router.get('/claims/:claimId', (req, res) => {
     let claimDetails = req.session.data.claims.find(claim => claim.id === req.params.claimId)
 
     res.render('claims/show', {
       claimDetails
     })
-})
+  })
+
 
 }
