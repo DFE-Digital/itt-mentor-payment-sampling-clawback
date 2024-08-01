478755b3e12107e13ceba18981b6c5aebae8d7d1
Tom <tommy_brown@me.com>
2024-08-01 16:10:33 +0100

    Added /sampling/export + review.html

--- app/routes/claims.js
+++ app/routes/claims.js
@@ -31,18 +31,65 @@ module.exports = router => {
       }) 
   }
 
-  res.render('claims/index', {
-    claims,
-    selectedFilters
+    res.render('claims/index', {
+      claims,
+      selectedFilters
 
+    })
   })
-})
 
   //Remove filter
   router.get('/claims/remove-status/:status', (req, res) => {
     _.set(req, 'session.data.filters.statuses',  _.pull(req.session.data.filters.statuses, req.params.status))
     res.redirect('/claims')
   })
+
+  //////////////////////////////////////
+  // Provider
+  /////////////////////////////////////
+
+  // Samples Filters filters[accreditedProvider]
+  router.get('/sampling', (req, res) => {
+    let claims = req.session.data.claims
+
+    let selectedProviderFilters = _.get(req.session.data.filters, 'accreditedProvider')
+
+    let selectedFilters = {
+    categories: []
+    }
+
+    if (_.get(selectedProviderFilters, 'length')) {
+        claims = claims.filter(claim => {
+        let matchesProvider = true
+        
+        matchesProvider = selectedProviderFilters.includes(claim.provider);
+
+        return matchesProvider
+    })
+
+    selectedFilters.categories.push({
+        heading: {text: 'status'},
+        items: selectedProviderFilters.map(label => {
+          return{
+            text: label,
+            href: `/sampling/remove-status/${label}`  
+          }
+        })
+      }) 
+}
+
+    res.render('sampling/index', {
+    claims,
+    selectedFilters
+
+    })
+})
+
+//Remove provider filter
+router.get('/sampling/remove-provider/:provider', (req, res) => {
+    _.set(req, 'session.data.filters.accreditedProvider',  _.pull(req.session.data.filters.accreditedProvider, req.params.provider))
+    res.redirect('/sampling')
+})
 
     
   //Claims list
--- app/routes/sampling.js
+++ app/routes/sampling.js
@@ -1,11 +1,13 @@
+const _ = require('lodash')
 module.exports = router => {
 
-router.get('/sampling/:claimId', (req, res) => {
-    let sample = req.session.data.claims.find(claim => claim.id === req.params.claimId)
+    //Samples main list
+    router.get('/sampling/:claimId', (req, res) => {
+        let sample = req.session.data.claims.find(claim => claim.id === req.params.claimId)
 
-    res.render('sampling/show', {
-        sample
+        res.render('sampling/show', {
+            sample
+        })
     })
-})
 
 }
